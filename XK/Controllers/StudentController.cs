2648641dec737d8c18ab14a623e7aa1bc8d23622
zhaokuo <1014336691@qq.com>
2016-04-02 19:21:18 +0800

    添加了几个页面, 经历了一场惊吓. git reset解决办法: 用 git reflog 找到丢失的版本, 再git reset  + git stash 回来,

---
+++ XK/Controllers/StudentController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XK.Models;
+
+namespace XK.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /SelectCourse/
+        xk_CourseDBContext course = new xk_CourseDBContext();
+
+        /// <summary>
+        /// 主页, 这里还没用到, 估计到时候直接返回login, 或者写一个一样的页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 选课
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SelectCourse()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 退课
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DeleteCourse()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 查看课程
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowCourse()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 查看课表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowTimetable()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 查看成绩
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowScore()
+        {
+            return View();
+        }
+    }
+}
